1864fc2dc44170158869bad027d1d63795d59701
Jan Pekar <jan.pekar@imatic.cz>
2019-08-18 14:42:43 +0200

    Execute someting after connect

--- startup.sh
+++ startup.sh
@@ -22,5 +22,5 @@ ipsec --status
 sleep 3
 
 # startup xl2tpd ppp daemon then send it a connect command
-(sleep 7 && echo "c myVPN" > /var/run/xl2tpd/l2tp-control) &
+(sleep 7 && echo "c myVPN" > /var/run/xl2tpd/l2tp-control && sleep 5 && eval '$VPN_POST_RUN') &
 exec /usr/sbin/xl2tpd -p /var/run/xl2tpd.pid -c /etc/xl2tpd/xl2tpd.conf -C /var/run/xl2tpd/l2tp-control -D
